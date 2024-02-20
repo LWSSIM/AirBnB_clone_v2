bd4e3f18ac245277923d7fa5a3fdc73f34ac08a4
LWSSIM <wassim.elmnajja2@gmail.com>
2024-02-20 15:38:18 +0100

    added test for console

---
+++ tests/test_console.py
@@ -0,0 +1,92 @@
+import uuid
+import unittest
+from io import StringIO
+from unittest.mock import patch
+from console import HBNBCommand
+import console
+
+
+class TestConsole(unittest.TestCase):
+    """TEST CONSOLE FUNCTIONALITY AND DOCs"""
+
+    def test_module_doc(self):
+        self.assertIsNotNone(console.__doc__)
+
+    def test_class_doc(self):
+        self.assertIsNotNone(HBNBCommand.__doc__)
+
+    def test_method_docs(self):
+        """Test all methods in ``console`` for docs"""
+        methods = [
+            HBNBCommand.do_EOF,
+            HBNBCommand.do_quit,
+            HBNBCommand.emptyline,
+            HBNBCommand.do_create,
+            HBNBCommand.do_show,
+            HBNBCommand.do_destroy,
+            HBNBCommand.do_all,
+            HBNBCommand.do_update,
+            HBNBCommand.default,
+        ]
+        for meth in methods:
+            self.assertIsNotNone(meth.__doc__)
+
+    def test_quit(self):
+        with patch("sys.stdout", new=StringIO()) as f:
+            HBNBCommand().onecmd("quit")
+            output = f.getvalue().strip()
+            self.assertEqual(output, "")
+
+    def test_EOF(self):
+        with patch("sys.stdout", new=StringIO()) as f:
+            HBNBCommand().onecmd("EOF")
+            output = f.getvalue().strip()
+            self.assertEqual(output, "")
+
+    def test_empty_line(self):
+        with patch("sys.stdout", new=StringIO()) as f:
+            HBNBCommand().onecmd("")
+            output = f.getvalue().strip()
+            self.assertEqual(output, "")
+
+    def test_help_create(self):
+        with patch("sys.stdout", new=StringIO()) as f:
+            HBNBCommand().onecmd("help create")
+            output = f.getvalue().strip()
+            self.assertIsNotNone(output)
+
+    def test_help_show(self):
+        with patch("sys.stdout", new=StringIO()) as f:
+            HBNBCommand().onecmd("help show")
+            output = f.getvalue().strip()
+            self.assertIsNotNone(output)
+
+    def test_help_destroy(self):
+        with patch("sys.stdout", new=StringIO()) as f:
+            HBNBCommand().onecmd("help destroy")
+            output = f.getvalue().strip()
+            self.assertIsNotNone(output)
+
+    def test_help_all(self):
+        with patch("sys.stdout", new=StringIO()) as f:
+            HBNBCommand().onecmd("help all")
+            output = f.getvalue().strip()
+            self.assertIsNotNone(output)
+
+    def test_help_update(self):
+        with patch("sys.stdout", new=StringIO()) as f:
+            HBNBCommand().onecmd("help update")
+            output = f.getvalue().strip()
+            self.assertIsNotNone(output)
+
+    def test_create_with_valid_class_name_BaseModel(self):
+        with patch("sys.stdout", new=StringIO()) as f:
+            HBNBCommand().onecmd("create BaseModel")
+            output = f.getvalue().strip()
+            try:
+                uuid.UUID(output)
+            except ValueError:
+                self.fail("Output is not a valid UUID")
+
+if __name__ == "__main__":
+        unittest.main()

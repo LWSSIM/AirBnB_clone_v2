745df9fd07ff0052b07a8ca9e937cc8ddf6c050e
LWSSIM <wassim.elmnajja2@gmail.com>
2024-02-21 20:17:21 +0100

    small fix in kwargs logic

--- models/base_model.py
+++ models/base_model.py
@@ -18,18 +18,22 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
-        if not kwargs:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-        else:
-            kwargs["updated_at"] = datetime.strptime(
-                kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f"
-            )
-            kwargs["created_at"] = datetime.strptime(
-                kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f"
-            )
-            del kwargs["__class__"]
+
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+        if kwargs:
+            if "updated_at" in kwargs:
+                kwargs["updated_at"] = datetime.strptime(
+                    kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f"
+                )
+            if "created_at" in kwargs:
+                kwargs["created_at"] = datetime.strptime(
+                    kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f"
+                )
+            if "__class__" in kwargs:
+                del kwargs["__class__"]
             self.__dict__.update(kwargs)
 
     def __str__(self):
@@ -49,13 +53,14 @@ class BaseModel:
         """Convert instance into dict format"""
         dictionary = {}
         dictionary.update(self.__dict__)
-        dictionary.update(
-            {"__class__": (str(type(self)).split(".")[-1]).split("'")[0]}
-        )
+        dictionary.update({"__class__": (str(type(self)).split(".")[-1]).split("'")[0]})
         dictionary["created_at"] = self.created_at.isoformat()
         dictionary["updated_at"] = self.updated_at.isoformat()
         if "_sa_instance_state" in dictionary:
-            del dictionary["_sa_instance_state"]
+            try:
+                del dictionary["_sa_instance_state"]
+            except Exception as e:
+                print(e)
         return dictionary
 
     def delete(self):

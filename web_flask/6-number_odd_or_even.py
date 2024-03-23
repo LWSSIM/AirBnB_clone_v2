6ba7021c39a9a3c067404bc374bb498039604c0d
LWSSIM <wassim.elmnajja2@gmail.com>
2024-03-23 02:41:35 +0000

    added 6-* temp/6-*

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -8,11 +8,15 @@
 #   /python/<text>: displat "Python", followed by text var,
 #   (text defaults to "is cool")
 #   /number/<n>: display “n is a number” only if n is an integer
+#   /number_template/<n>: display a HTML page only if n is int
+#       -- H1 tag:"Number:n" inside BODY tag
+#   /number_odd_or_even/<n>: display a HTML page only if n is an integer:
+#       -- H1 tag: “Number: n is even|odd” inside the tag BODY
 # You must use the option strict_slashes=False in your route definition
 
 
 from flask import Flask
-
+from flask import render_template
 
 app = Flask(__name__)
 
@@ -41,6 +45,16 @@ def python_text(text):
 @app.route("/number/<int:n>", strict_slashes=False)
 def number(n):
     return "{} is a number".format(n)
+
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def number_template(n):
+    return render_template("5-number.html", n=n)
+
+
+@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
+def number_odd_or_even(n):
+    return render_template("6-number_odd_or_even.html", n=n)
 
 
 if __name__ == "__main__":

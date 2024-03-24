5f2bd47b0fbd6153e3d223dfc5e5a5199a5d03f4
LWSSIM <wassim.elmnajja2@gmail.com>
2024-03-24 16:53:31 +0000

    added 8-*

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -19,40 +19,41 @@ from flask import Flask
 from flask import render_template
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
-@app.route("/", strict_slashes=False)
+@app.route("/")
 def hello_hbnb():
     return "Hello HBNB!"
 
 
-@app.route("/hbnb", strict_slashes=False)
+@app.route("/hbnb")
 def hbnb():
     return "HBNB"
 
 
-@app.route("/c/<text>", strict_slashes=False)
+@app.route("/c/<text>")
 def c_text(text):
     return "C {}".format(text.replace("_", " "))
 
 
-@app.route("/python/", defaults={"text": "is cool"}, strict_slashes=False)
+@app.route("/python/", defaults={"text": "is cool"})
 @app.route("/python/<text>", strict_slashes=False)
 def python_text(text):
     return "Python {}".format(text.replace("_", " "))
 
 
-@app.route("/number/<int:n>", strict_slashes=False)
+@app.route("/number/<int:n>")
 def number(n):
     return "{} is a number".format(n)
 
 
-@app.route("/number_template/<int:n>", strict_slashes=False)
+@app.route("/number_template/<int:n>")
 def number_template(n):
     return render_template("5-number.html", n=n)
 
 
-@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
+@app.route("/number_odd_or_even/<int:n>")
 def number_odd_or_even(n):
     return render_template("6-number_odd_or_even.html", n=n)
 
---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""listen to a port and load all cities of a state
+with teardown function
+"""
+
+
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+from models.city import City
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/cities_by_states')
+def cities_by_states():
+    """display a HTML page: (inside the tag BODY)"""
+    states = storage.all(State)
+    states = states.values()
+    return render_template('8-cities_by_states.html',
+                           states=states)
+
+
+@app.teardown_appcontext
+def teardown_session(error):
+    storage.close()
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)

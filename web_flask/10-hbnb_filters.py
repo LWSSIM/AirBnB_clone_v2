f91a0110657349b32d8a4ad3ffaaf773c522ed8b
LWSSIM <wassim.elmnajja2@gmail.com>
2024-03-25 18:26:12 +0000

    added all for 10-* working version*

---
+++ web_flask/10-hbnb_filters.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""" Starts a Flask web application 
+add filters to the HBNB app
+"""
+
+
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+from models.amenity import Amenity
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/hbnb_filters')
+def hbnb_filters():
+    """Display a HTML page like 6-index.html as a template"""
+    states = storage.all(State)
+    amenities = storage.all(Amenity)
+    return render_template('10-hbnb_filters.html', states=states, amenities=amenities)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """Remove the current SQLAlchemy Session"""
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)

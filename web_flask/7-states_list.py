cfbeadf1942974be466a2895916272c348699a81
LWSSIM <wassim.elmnajja2@gmail.com>
2024-03-25 00:53:27 +0000

    trying maybe

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -12,7 +12,43 @@ from models.state import State
 app = Flask(__name__)
 
 
-@app.route('/states_list', strict_slashes=False)
+@app.route("/")
+def hello_hbnb():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def hbnb():
+    return "HBNB"
+
+
+@app.route("/c/<text>")
+def c_text(text):
+    return "C {}".format(text.replace("_", " "))
+
+
+@app.route("/python/", defaults={"text": "is cool"})
+@app.route("/python/<text>", strict_slashes=False)
+def python_text(text):
+    return "Python {}".format(text.replace("_", " "))
+
+
+@app.route("/number/<int:n>")
+def number(n):
+    return "{} is a number".format(n)
+
+
+@app.route("/number_template/<int:n>")
+def number_template(n):
+    return render_template("5-number.html", n=n)
+
+
+@app.route("/number_odd_or_even/<int:n>")
+def number_odd_or_even(n):
+    return render_template("6-number_odd_or_even.html", n=n)
+
+
+@app.route('/states_list')
 def states_list():
     """display a HTML page: (inside the tag BODY)"""
     states = storage.all(State).values()
